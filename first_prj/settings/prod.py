e021d8598927cda59493986a939ab0e19be8c295
Your Name <you@your-domain.com>
2020-08-31 15:45:04 +0900

    commit test

--- first_prj/settings/prod.py
+++ first_prj/settings/prod.py
@@ -7,10 +7,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mysqltutorial',
-        'USER' : 'root',
+        'USER': 'codestates',
         'PASSWORD': config('DEPLOY_DATABSE_PASSWORD'),
-        'PORT' : 3306,
-        'HOST': '127.0.0.1',
+        'PORT': 3306,
+        'HOST': 'database-1.c9ohwqemljbd.ap-northeast-2.rds.amazonaws.com',
         'OPTIONS': {
             'charset': 'utf8mb4',  # 데이터베이스 한글도 저장하게 하게하기
         }
