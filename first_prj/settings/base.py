897974e7296e699db1ecde9a7f306e7c95ff3a16
Your Name <you@your-domain.com>
2020-08-31 15:42:54 +0900

    deploy error test

--- first_prj/settings/base.py
+++ first_prj/settings/base.py
@@ -33,7 +33,7 @@ SECRET_KEY = 'badhashing'
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', '.amazonaws.com']
 
 
 # Application definition
@@ -91,19 +91,7 @@ WSGI_APPLICATION = 'first_prj.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysqltutorial',
-        'USER' : 'root',
-        'PASSWORD': config('DATABSE_PASSWORD'),
-        'PORT' : 3306,
-        'HOST': '127.0.0.1',
-        'OPTIONS': {
-            'charset': 'utf8mb4',  # 데이터베이스 한글도 저장하게 하게하기
-        }
-    }
-}
+
 
 # 개발환경 CORS setting
 CORS_ORIGIN_ALLOW_ALL = True
--- first_prj/settings/prod.py
+++ first_prj/settings/prod.py
@@ -2,3 +2,17 @@ from .base import *
 
 DEBUG = False
 SECRET_KEY = config('SECRET_KEY')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mysqltutorial',
+        'USER' : 'root',
+        'PASSWORD': config('DEPLOY_DATABSE_PASSWORD'),
+        'PORT' : 3306,
+        'HOST': '127.0.0.1',
+        'OPTIONS': {
+            'charset': 'utf8mb4',  # 데이터베이스 한글도 저장하게 하게하기
+        }
+    }
+}
